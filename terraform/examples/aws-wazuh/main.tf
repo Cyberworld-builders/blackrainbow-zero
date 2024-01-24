f24d3d8956a7907bfe51b80da4679b7a6e28fe30
Jay <jaylong255@gmail.com>
2024-01-24 08:00:33 +0000

    adding role, policy and attachment for the instance

--- terraform/examples/aws-wazuh/main.tf
+++ terraform/examples/aws-wazuh/main.tf
@@ -1,3 +1,78 @@
 provider "aws" {
   region = var.aws_region
 }
+
+# instance profile
+resource "aws_iam_instance_profile" "wazuh" {
+  name = "wazuh"
+  role = aws_iam_role.wazuh.name
+}
+
+# role
+resource "aws_iam_role" "wazuh" {
+  name = "wazuh"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# role policy
+resource "aws_iam_role_policy" "wazuh" {
+  name = "wazuh"
+  role = aws_iam_role.wazuh.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceAttribute",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVpcs",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVolumeAttribute",
+        "ec2:DescribeVolumeStatus",
+        "ec2:DescribeVolumeAttribute",
+        "ec2:DescribeVolumeStatus",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSnapshotAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeKeyPairs",
+        "ec2:DescribeTags",
+        "ec2:DescribeAddresses",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DescribeNetworkInterfaceAttribute",
+        "ec2:DescribeNetworkInterfacePermissions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeInternetGateways",
+        "ec2:DescribeVpcAttribute",
+        "ec2:DescribeVpcEndpoints",
+        "ec2:DescribeVpcEndpointAttribute",
+        "ec2:DescribeVpcPeeringConnections",
+        "ec2:DescribeVpnGateways",
+        "ec2:DescribeVpnConnections"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+    }
+    ]
+}
+EOF
+}
