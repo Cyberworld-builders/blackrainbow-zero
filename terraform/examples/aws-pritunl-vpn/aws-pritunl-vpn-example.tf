c563d63ba9cd6b54bd29b8a76336deceeeb259a5
Jay <jaylong255@gmail.com>
2024-02-05 20:25:45 +0000

    destroying old tfcloud based workflow

--- terraform/examples/aws-pritunl-vpn/aws-pritunl-vpn-example.tf
+++ terraform/examples/aws-pritunl-vpn/aws-pritunl-vpn-example.tf
@@ -1,6 +1,8 @@
 module "pritunl_vpn" {
     source = "../../modules/aws-pritunl"
 
+    count = 0
+
     environment = "example"
     vpc_id = "vpc-02d8e874d887624c0"
     subnet_id = "subnet-0cb88233e387c955a"
