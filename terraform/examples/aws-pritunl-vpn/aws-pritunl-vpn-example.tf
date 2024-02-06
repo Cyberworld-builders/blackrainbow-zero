5dbd6e520c99dedc53f08410b713b2187f1428da
Jay <jaylong255@gmail.com>
2024-02-06 16:03:40 +0000

    condensing example files, switching to bucket for examples

--- terraform/examples/aws-pritunl-vpn/aws-pritunl-vpn-example.tf
+++ terraform/examples/aws-pritunl-vpn/aws-pritunl-vpn-example.tf
@@ -1,5 +1,20 @@
-module "vpn" {
+# A Simple AWS provider in the us-east-1 region with a local backend
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  # use s3 for the backend. the bucket is called "kompliant-terraform-examples"
+  backend "s3" {
+    bucket = "derpys-playground-tf-examples"
+    key    = "pritunl.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "aws-pritunl" {
     source = "../../modules/aws-pritunl"
+    # source = "github.com/cyberworld-builders/blackrainbow-zero//terraform/modules/aws-pritunl?ref=main"
 
     # count = 0
 
