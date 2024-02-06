ff0fa29ecbbef34fb5929c102299c4030d524766
Jay Long <jay@cyberworldbuilders.com>
2024-02-06 13:34:58 -0600

    documenting usage (#17)

--- terraform/examples/aws-pritunl-vpn/variables.tf
+++ terraform/examples/aws-pritunl-vpn/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  type        = string
-  default     = "us-east-2"
-  description = "AWS region for all resources"
-}
-
 variable "server_ports" {
   type        = list(number)
   default     = [13806]
